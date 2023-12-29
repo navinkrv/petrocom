7bf3de94545924032d62be812b8cedd91c4d2a5f
navin kumar <navinkrv@gmail.com>
2023-12-29 21:05:35 +0530

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -32,7 +32,7 @@ Route::post("/user/login", [UserController::class, "login"]);
 Route::get("/user/getUserData", [UserController::class, "getUserData"])->middleware("auth:sanctum");
 Route::post("/user/createAdminAccount", [UserController::class, "createAdminAccount"])->middleware("auth:sanctum")->middleware(SAdmin::class);
 Route::post("/user/updateAdminPassword", [UserController::class, "updateAdminPassword"])->middleware("auth:sanctum")->middleware(SAdmin::class);
-Route::post("/user/listAdminAccount", [UserController::class, "listAdminAccount"])->middleware("auth:sanctum")->middleware(SAdmin::class);
+Route::get("/user/listAdminAccount", [UserController::class, "listAdminAccount"])->middleware("auth:sanctum")->middleware(SAdmin::class);
 
 //client
 Route::post("/client/createClient", [ClientDetailController::class, "createClient"])->middleware("auth:sanctum")->middleware(userTypeAuth::class);
