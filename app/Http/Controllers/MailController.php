e76a247aa42a0222aad0ecc92608a2b0d999c3b6
navin kumar <navinkrv@gmail.com>
2024-01-14 17:19:42 +0530

    mailer new

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -13,64 +13,66 @@ class MailController extends Controller
 {
     public function sendLoad(Request $request)
     {
-        $validation = $request->validate([
-            "name" => "required",
-            "email" => "required",
-            "frieght_type" => "required",
-            "multidrop" => "required",
-            "message" => "required"
-        ]);
+        // $validation = $request->validate([
+        //     "name" => "required",
+        //     "email" => "required",
+        //     "frieght_type" => "required",
+        //     "multidrop" => "required",
+        //     "message" => "required"
+        // ]);
+        $validation = true;
 
 
         if ($validation) {
             // echo $request;
-
+            $job_location_formatted = str_replace("},{", "}   ,   {", $request->job_location_data);
             $message = "
-                <table>
+
+                <table style = 'border:1px solid black;'>
 <tbody>
-<tr>
-<td>name</td>
-<td>{$request->name}</td>
+<tr >
+<td style = 'border:1px solid black; padding:5px;' >Name</td>
+<td style = 'border:1px solid black; padding:5px;' >{$request->name}</td>
 </tr>
 <tr>
-<td>email</td>
-<td>{$request->email}</td>
+<td style = 'border:1px solid black; padding:5px;'>Email</td>
+<td style = 'border:1px solid black; padding:5px;'>{$request->email}</td>
 </tr>
 <tr>
-<td>phone</td>
-<td>{$request->phone}</td>
+<td style = 'border:1px solid black; padding:5px;'>Phone</td>
+<td style = 'border:1px solid black; padding:5px;'>{$request->phone}</td>
 </tr>
 <tr>
-<td>Frieght type</td>
-<td>{$request->frieght_type}</td>
+<td style = 'border:1px solid black; padding:5px;'>Frieght type</td>
+<td style = 'border:1px solid black; padding:5px;'>{$request->frieght_type}</td>
 </tr>
 <tr>
-<td>multidrop</td>
-<td>{$request->multidrop}</td>
+<td style = 'border:1px solid black; padding:5px;'>Multidrop</td>
+<td style = 'border:1px solid black; padding:5px;'>{$request->multidrop}</td>
 </tr>
 <tr>
-<td>destination</td>
-<td>{$request->job_location_data}</td>
+<td style = 'border:1px solid black; padding:5px;'>Location Data</td>
+<td style = 'border:1px solid black; padding:5px;'>{$job_location_formatted}</td>
 </tr>
 <tr>
-<td>length</td>
-<td>{$request->length}</td>
+<td style = 'border:1px solid black; padding:5px;'>Length</td>
+<td style = 'border:1px solid black; padding:5px;'>{$request->length}</td>
 </tr>
 <tr>
-<td>breadth</td>
-<td>{$request->breadth}</td>
+<td style = 'border:1px solid black; padding:5px;'>Breadth</td>
+<td style = 'border:1px solid black; padding:5px;'>{$request->breadth}</td>
 </tr>
 <tr>
-<td>height</td>
-<td>{$request->height}</td>
+<td style = 'border:1px solid black; padding:5px;'>Height</td>
+<td style = 'border:1px solid black; padding:5px;'>{$request->height}</td>
 </tr>
 <tr>
-<td>weight</td>
-<td>{$request->weight}</td>
+<td style = 'border:1px solid black; padding:5px;'>Weight</td>
+<td style = 'border:1px solid black; padding:5px;'>{$request->weight}</td>
 </tr>
 <tr>
-<td>message</td>
-<td>{$request->message}</td>
+<td style = 'border:1px solid black; padding:5px;'>Message</td>
+<td style = 'border:1px solid black; padding:5px;'>{$request->message}</td>
 </tr>
 </tbody>
 </table>
